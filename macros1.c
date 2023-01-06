4aeaa30ef55eb7b10c6f4ca1b20667f2068d37f7
IlyaGabyshev <gabapokerman@gmail.com>
2023-01-06 19:15:18 +0300

    hours&minutes in minutes

---
+++ macros1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define time2min(a,b) ((a)*60+b)
+
+int main(void)
+{
+   int h, m, mm;
+scanf("%d:%d", &h, &m);
+
+mm = time2min(h, m);
+printf("%d\n", mm);
+
+mm = time2min(h, m)*2;
+printf("%d\n", mm);
+
+mm = time2min(h+1, m+5);
+printf("%d\n", mm);
+}
